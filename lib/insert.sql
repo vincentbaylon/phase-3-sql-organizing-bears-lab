08e1f127cd0f3b4b7c0d2097a760674fe6a73be4
vincentbaylon <vincentbaylon@gmail.com>
2021-08-12 16:15:26 -0700

    commit tree e3137759432b2d4522517c4d49f09fe469171815

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES  ("Mr. Chocolate", 23, "M", "brown", "aggressive", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES  ("Rowdy", 15, "M", "red", "goofy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES  ("Tabitha", 6, "F", "dark brown", "goofy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES  ("Sergeant Brown", 35, "M", "black", "aggressive", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES  ("Melissa", 13, "F", "purple", "happy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES  ("Grinch", 40, "M", "black", "grumpy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES  ("Wendy", 6, "F", "white", "happy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES  (NULL, 22, "M", "dark grey", "aggressive", true);
